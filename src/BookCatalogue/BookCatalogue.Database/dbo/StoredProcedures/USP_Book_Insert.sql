ce09b0144321ca5744c5df912990a29fcbc5555f
Yurii Nosach <nosach.yurko@gmail.com>
2018-07-26 10:17:12 +0300

    Added custom type, improved procedures and tables.

---
+++ src/BookCatalogue/BookCatalogue.Database/dbo/StoredProcedures/USP_Book_Insert.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[USP_Book_Insert]
+	@Title NVARCHAR(64),
+	@Pages INT,
+	@Rating INT,
+	@PublishedDate DATE,
+	@AuthorsIds IntArrayType READONLY
+AS
+
+BEGIN TRANSACTION
+
+	DECLARE @InsertedBook TABLE (Id INT)
+	DECLARE @InsertedBookId INT
+
+	INSERT INTO [Book]([Title], [Pages], [Rating], [PublishedDate])
+		OUTPUT INSERTED.Id INTO @InsertedBook
+		VALUES (@Title, @Pages, @Rating, @PublishedDate)
+
+	SET @InsertedBookId = (SELECT TOP 1 [Id] FROM @InsertedBook)
+
+	INSERT INTO [BookAuthor]([BookId], [AuthorId])
+		SELECT [b].[Id], [a].[Item]
+		FROM @InsertedBook AS [b], @AuthorsIds AS [a]
+
+COMMIT TRANSACTION
+
+RETURN @InsertedBookId
---
+++ src/BookCatalogue/BookCatalogue.Database/dbo/StoredProcedures/USP_Book_Update.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[USP_Book_Update]
+	@Id INT,
+	@Title NVARCHAR(64),
+	@Pages INT,
+	@Rating INT,
+	@PublishedDate DATE,
+	@AuthorsIds IntArrayType READONLY
+AS
+	
+BEGIN TRANSACTION
+	UPDATE [Book] SET
+		[Title] = @Title,
+		[Pages] = @Pages,
+		[Rating] = @Rating,
+		[PublishedDate] = @PublishedDate
+	WHERE [Book].[Id] = @Id
+
+	DELETE FROM [BookAuthor]
+	WHERE [BookAuthor].[BookId] = @Id AND [BookAuthor].[AuthorId] NOT IN (SELECT * FROM @AuthorsIds)
+
+	DECLARE @authorsToInsert IntArrayType
+
+	INSERT INTO @authorsToInsert
+	SELECT [Item] FROM @AuthorsIds WHERE [Item] NOT IN (SELECT [ba].[AuthorId] FROM [BookAuthor] AS [ba] WHERE [ba].[BookId] = @Id)
+
+	INSERT INTO [BookAuthor]([BookId], [AuthorId])
+		SELECT @Id, [a].[Item]
+		FROM @authorsToInsert AS [a]
+
+COMMIT TRANSACTION
+RETURN 0
